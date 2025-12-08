2108cbcf8cd10218ac00cab3c3d345fbb8a84205
david-kindl <davidkindl92@gmail.com>
2025-12-08 10:14:29 +0100

    Finished part1

--- day8.py
+++ day8.py
@@ -1,5 +1,9 @@
-from collections import OrderedDict
+from collections import defaultdict, deque
 from itertools import combinations
+from operator import mul
+from functools import reduce
+from typing import Literal
+from sys import argv
 
 
 TEST = """162,817,812
@@ -24,6 +28,18 @@ TEST = """162,817,812
 425,690,689"""
 
 
+def get_boxes(io: str, mode: Literal["file", "text"] = "file") -> set[tuple[int, ...]]:
+    match mode.lower():
+        case "file":
+            with open(io, mode="r", encoding="utf8") as fl:
+                raw_data = fl.read()
+        case "text":
+            raw_data = io
+        case _:
+            raise ValueError(f"Unknown mode '{mode}'.")
+    return {tuple(map(int, box.split(","))) for box in raw_data.split("\n")}
+
+
 def euclidean_distance(p, q) -> int:
     if len(p) == len(q):
         return sum((p[n] - q[n]) ** 2 for n in range(len(p))) ** (1 / 2)
@@ -31,16 +47,49 @@ def euclidean_distance(p, q) -> int:
         raise ValueError(f"Length of {p} and {q} are not equal.")
 
 
-def get_distance_rank(boxes, n: int):
+def get_distance_rank(boxes, n: int) -> dict[int, list[tuple[int, ...]]]:
     distances = {}
     for box1, box2 in combinations(boxes, 2):
         distance = euclidean_distance(box1, box2)
         distances[distance] = sorted([box1, box2])
-    return OrderedDict(sorted(distances.items())[:n])
+    return dict(sorted(distances.items())[:n])
+
+
+def build_graph(boxes: dict[int, list[tuple[int, ...]]]) -> defaultdict[tuple[int, ...], list[tuple[int, ...]]]:
+    d = defaultdict(list)
+    for distance, pairs in boxes.items():
+        cord1, cord2 = pairs
+        d[cord1].append(cord2)
+        d[cord2].append(cord1)
+    return d
+
+
+def get_circuits(graph: defaultdict[tuple[int, ...], list[tuple[int, ...]]]):
+    def bfs():
+        visited = set()
+        circuits = []
+
+        for node in graph:
+            if node not in visited:
+                queue = deque([node])
+                circuit = set()
+
+                while queue:
+                    n = queue.popleft()
+                    if n in visited:
+                        continue
+                    visited.add(n)
+                    circuit.add(n)
+                    queue.extend(graph[n])
+                circuits.append(circuit)
+        return circuits
+
+    circuits = bfs()
+    return sorted([circuit for circuit in circuits], key=lambda x: len(x), reverse=True)
 
 
 if __name__ == "__main__":
-    junction_boxes = {tuple(map(int, box.split(","))) for box in TEST.split("\n")}
-    d = get_distance_rank(junction_boxes, 10)
-    for k, v in d.items():
-        print(k, v)
+    junction_boxes = get_boxes(*argv[1:])
+    distances = get_distance_rank(junction_boxes, 1000)
+    graph = build_graph(distances)
+    print(reduce(mul, [len(circuit) for circuit in get_circuits(graph)[:3]]))
